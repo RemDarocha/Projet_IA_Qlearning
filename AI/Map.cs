c5347c5be5a08e51ace80a4531614f24377c49e2
RemDarocha <remi.darocha@hotmail.fr>
2019-06-26 07:53:19 +0200

    Refonte de "map"
    Parce que j'ai fait de la merde

--- AI/Map.cs
+++ AI/Map.cs
@@ -4,7 +4,51 @@ using System.Text;
 
 namespace AI
 {
-    class map
+    class Map
     {
+        List<string> map = new List<string>();
+        Player player = new Player();
+        public void Start()
+        {
+            Initialisation();
+        }
+        public void Initialisation()
+        {
+            map.Add("xxxxxx");
+            map.Add("x----x");
+            map.Add("x----x");
+            map.Add("x----x");
+            map.Add("x----x");
+            map.Add("x----x");
+            map.Add("x----x");
+            map.Add("xxxxxx");
+        }
+
+        public void Display()
+        {
+            Console.Clear();
+            foreach (string line in map)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        public enum Movement
+        {
+            Right = 0,
+            Up = 1,
+            Left = 2,
+            Down = 3
+        }
+
+        public Tuple<string, int> Move(Movement movement)
+        {
+            int x = player.position.x;
+            int y = player.position.y;
+
+
+
+        }
+
     }
 }
