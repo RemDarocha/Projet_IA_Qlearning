5744e22175361812d2047badc564e9bf867781a3
RemDarocha <remi.darocha@hotmail.fr>
2019-06-26 07:59:10 +0200

    update 2 map

--- AI/Map.cs
+++ AI/Map.cs
@@ -15,12 +15,12 @@ namespace AI
         public void Initialisation()
         {
             map.Add("xxxxxx");
+            map.Add("x---Rx");
+            map.Add("x-x--x");
+            map.Add("x---xx");
+            map.Add("xxx--x");
             map.Add("x----x");
-            map.Add("x----x");
-            map.Add("x----x");
-            map.Add("x----x");
-            map.Add("x----x");
-            map.Add("x----x");
+            map.Add("x0---x");
             map.Add("xxxxxx");
         }
 
@@ -43,10 +43,29 @@ namespace AI
 
         public Tuple<string, int> Move(Movement movement)
         {
-            int x = player.position.x;
+            Tuple<string, int> ret = new Tuple();
+            int x = player.position.x - 2;
             int y = player.position.y;
 
-
+            
+            switch (movement)
+            {
+                case Movement.Right:
+                    player.position.x++;
+                    break;
+                case Movement.Up:
+                    player.position.y++;
+                    break;
+                case Movement.Left:
+                    player.position.x--;
+                    break;
+                case Movement.Down:
+                    player.position.y--;
+                    break;
+            }
+        }
+        private void modifyMap(int x, int y, char c)
+        {
 
         }
 
